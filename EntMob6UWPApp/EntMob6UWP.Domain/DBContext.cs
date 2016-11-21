bdd9a17f391e20cde947074da2601a49eddcf027
Lanssens <tlanssens@gmail.com>
2016-11-21 16:14:52 +0100

    Added New Models + DBContext change
    Added New Models + DBContext change

--- EntMob6UWPApp/EntMob6UWP.Domain/DBContext.cs
+++ EntMob6UWPApp/EntMob6UWP.Domain/DBContext.cs
@@ -10,6 +10,9 @@ namespace EntMob6UWP.Domain
     class DBContext : DbContext
     {
         public DbSet<Humidity> humidities { get; set; }
+        public DbSet<Brightness> brightnesses { get; set; }
+        public DbSet<Temperature> temperatures { get; set; }
+        public DbSet<AirPressure> airPressures { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlite("Filename=db.db");
@@ -20,6 +23,21 @@ namespace EntMob6UWP.Domain
                 .HasKey(b => b.ID);
             modelBuilder.Entity<Humidity>()
                 .Property(b => b.Percentage).HasColumnName("percentage");
+
+            modelBuilder.Entity<Brightness>()
+               .HasKey(b => b.ID);
+            modelBuilder.Entity<Brightness>()
+                .Property(b => b.Value).HasColumnName("value");
+
+            modelBuilder.Entity<Temperature>()
+               .HasKey(b => b.ID);
+            modelBuilder.Entity<Temperature>()
+                .Property(b => b.Value).HasColumnName("value");
+
+            modelBuilder.Entity<AirPressure>()
+               .HasKey(b => b.ID);
+            modelBuilder.Entity<AirPressure>()
+                .Property(b => b.Value).HasColumnName("value");
 
         }
     }
---
+++ EntMob6UWPApp/EntMob6UWP.Domain/Temperature.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntMob6UWP.Domain
+{
+    class Temperature
+    {
+        public string ID { get; set; }
+
+        public double Value { get; set; }
+        [JsonProperty(PropertyName = "measured")]
+        [JsonConverter(typeof(MilisecondEpochConverter))]
+        public DateTime Measured { get; set; }
+    }
+}
