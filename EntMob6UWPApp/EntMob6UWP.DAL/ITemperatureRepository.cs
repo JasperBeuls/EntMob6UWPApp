0e1be7c39deb8ff1445075968ba57d05e63ab24c
Lanssens <tlanssens@gmail.com>
2016-11-22 14:52:45 +0100

    Added Temperature repository and data service

---
+++ EntMob6UWPApp/EntMob6UWP.DAL/ITemperatureRepository.cs
@@ -0,0 +1,19 @@
+﻿using EntMob6UWP.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntMob6UWP.DAL
+{
+    public interface ITemperatureRepository
+    {
+        Temperature GetLatestTemperature();
+        List<Temperature> GetAllTemperatures();
+        List<TemperatureAverage> GetAllTemperatureAveragesMinute();
+        List<TemperatureAverage> GetAllTemperatureAveragesHour();
+        List<TemperatureAverage> GetAllTemperatureAveragesDay();
+        List<TemperatureAverage> GetAllTemperatureAveragesMonth();
+    }
+}
---
+++ EntMob6UWPApp/EntMob6UWPApp/Services/ITemperatureDataService.cs
@@ -0,0 +1,18 @@
+﻿using EntMob6UWP.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntMob6UWPApp.Services
+{
+    public interface ITemperatureDataService
+    {
+
+        List<Temperature> GetAllTemperatures();
+        Temperature GetLatestTemperature();
+    }
+
+
+}
